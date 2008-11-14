5b910cd2f30d754496c915b4144d8ff9c402d61a
Kyle Hendricks <kyleh@mendtechnologies.com>
2008-11-14 13:51:38 -0700

    Changed settings page to resemble skydeck - added password type to password field - added image from skydeck settings page

--- application/views/settings/settings_view.php
+++ application/views/settings/settings_view.php
@@ -1,36 +1,32 @@
 <?php echo $this->load->view('_common/header'); ?>
 <div id="content">
 	<h1><?php echo $heading ?></h1>
-
+		
+		<p>To use this application you need to configure your FreshBooks settings and Tick settings. You can get these settings from inside your FreshBooks account. Please follow the directions below if you aren't exactly sure on how to activate the FreshBooks API.</p>
+	
+	<h3>Log into your Freshbooks Account</h3> 
+	<p>Click <strong>Settings</strong> then <strong>Enable FreshBooks API</strong>. Once you've enabled the API, you will see <strong>Your API URL</strong> & <strong>Your Authentication Token</strong> in the middle of that page. You'll need to enter those here to continue. </p>
+	<h3>Tick Settings</h3>
+	<p>Your Tick email and password were sent to you when you opened your account.</p>
+	<img src="<?php echo(base_url()); ?>public/images/settings.jpg" alt="FreshBooks screenshot of API settings page." style="float:right; border: 3px solid rgb(201, 201, 201); margin-left: 50px; margin-top: 10px;" />
+	
 	<?php echo form_open('settings')."\n"; ?>
 	<div id="apiform">
 		<div class="api-input">
           <label>FreshBooks API URL</label>
 			<input class="input" type="text" name="fburl" value="<?php echo $fburl ? $fburl : $this->validation->fburl; ?>" size="50" />
-		<div class="form-note">
-            <p><strong>To Add Your FreshBooks API URL:</strong>  1. Login to FreshBooks 2. Click the 'Settings' link at the top of the header. 3. Under 'Step 3: Third Party Services' click the 'Enable FreshBooks API' link. 4. Check the 'Yes, I agree to the API terms of service' checkbox and your API URL and Token will be available.  Copy and paste the API URL into the form field.</p>
-          </div>
         </div>
 		<div class="api-input">
           <label>FreshBooks Token</label>
 			<input class="input" type="text" name="fbtoken" value="<?php echo $fbtoken ? $fbtoken : $this->validation->fbtoken; ?>" size="50" />
-		<div class="form-note">
-            <p><strong>To Add Your FreshBooks API Token:</strong>  1. Login to FreshBooks 2. Click the 'Settings' link at the top of the header. 3. Under 'Step 3: Third Party Services' click the 'Enable FreshBooks API' link. 4. Check the 'Yes, I agree to the API terms of service' checkbox and your API URL and Token will be visible. Copy and paste the API token into this form field.</p>
-          </div>
         </div>
 		<div class="api-input">
           <label>Tick Email Address</label>
 			<input class="input" type="text" name="tsemail" value="<?php echo $tsemail ? $tsemail : $this->validation->tsemail; ?>" size="50" />
-		<div class="form-note">
-            <p><strong>To Add Your Tick Email Address:</strong></p>
-          </div>
         </div>
 		<div class="api-input">
           <label>Tick Password</label>
-			<input class="input" type="text" name="tspassword" value="<?php echo $tspassword ? $tspassword : $this->validation->tspassword; ?>" size="50" />
-		<div class="form-note">
-            <p><strong>To Add Your Tick Password:</strong></p>
-          </div>
+			<input class="input" type="password" name="tspassword" value="<?php echo $tspassword ? $tspassword : $this->validation->tspassword; ?>" size="50" />
         </div>
 		<input class="submit" type="submit" name="submit" value="<?php echo $submitname ?>" />
 	</div>
